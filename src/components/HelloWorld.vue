99603efde5819f4f67f04a00037d2f0ecb2dc41a
Wighawag <wighawag@gmail.com>
2020-08-20 13:57:39 +0100

    firxt commit with globals and builtins setup but failing at runtime

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement. {{ randomBytes() }}</p>
+</template>
+
+<script>
+import {randomBytes} from "@ethersproject/random";
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
